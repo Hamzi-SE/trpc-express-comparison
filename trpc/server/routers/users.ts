83c22d3abf77862cd0205c01109d25c7b4b0aa35
Hamzi-SE <hamzii.se@gmail.com>
2023-04-17 23:33:51 +0500

    first commit

---
+++ trpc/server/routers/users.ts
@@ -0,0 +1,26 @@
+import { t } from '../trpc'
+import { z } from 'zod'
+import { randomUUID } from 'crypto'
+
+type User = {
+	id: string
+	name: string
+	age: number
+}
+
+const USERS: User[] = [
+	{ id: '1', name: 'Hamza', age: 27 },
+	{ id: '2', name: 'Ali', age: 45 },
+]
+
+export const usersRouter = t.router({
+	byId: t.procedure.input(z.string()).query(req => {
+		return USERS.find(user => user.id === req.input)
+	}),
+	create: t.procedure.input(z.object({ name: z.string(), age: z.number() })).mutation(req => {
+		const { name, age } = req.input
+		const user: User = { id: randomUUID(), name, age }
+		USERS.push(user)
+		return user
+	}),
+})
